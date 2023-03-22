73a0ce4d550d0b5eac212a4ed04085ae6f8804a9
XietrZ <sxietrz@yahoo.com>
2023-03-22 19:46:16 +0800

    Create firebaseDatabase.js

---
+++ src/database/firebaseDatabase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-f697ETbbY0cFrzV0zhVJvHanui-2XmE",
+  authDomain: "grab-clone-sacedor.firebaseapp.com",
+  projectId: "grab-clone-sacedor",
+  storageBucket: "grab-clone-sacedor.appspot.com",
+  messagingSenderId: "31174955848",
+  appId: "1:31174955848:web:60e34deb886d29e0bd597d",
+  measurementId: "G-SEBFMH4Q6Q",
+};
